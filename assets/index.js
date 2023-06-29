b3e19e19aad87b0618886ac80dcb86a3c5b47d43
devkn <kimoueric04@gmail.com>
2023-06-29 09:43:53 +0000

    ajustements mineurs (font-size , line-clamp : 2

--- assets/index.js
+++ assets/index.js
@@ -20,12 +20,13 @@ async function useData() {
 
         row.innerHTML += `
           <div class="col-12 col-sm-6 col-md-auto col-md-4 col-lg-3">
-            <div class="food d-flex justify-content-center flex-column align-items-center ">
+            <div class="food d-flex  justify-content-center flex-column align-items-center ">
               <div>
                 <img src="${item.strCategoryThumb}" alt="image" class="h-100 w-100" />
               </div>
-              <div class="d-flex justify-content-center">
-                <h5 class="mx-2">Categorie</h5>:<h5 class="mx-2">${item.strCategory}</h5>
+              <div class="d-flex justify-content-center ti">
+              
+                <h5 class=" ">Categorie</h5><h5 class='center'>:</h5><h5 class="">${item.strCategory}</h5>
               </div>
               <div class="alert alert-light">
                 <div class="form-text text-center p-1 text-dark ">
